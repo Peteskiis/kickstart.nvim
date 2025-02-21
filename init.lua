44d2ee12995370607ed29e8c4e1b095a1f6b9bc8
Peter <peter@clusterbase.ai>
2025-02-20 19:25:16 -0500

    init.lua

--- init.lua
+++ init.lua
@@ -965,7 +965,7 @@ indent = { enable = false, disable = { 'typescript' } },
 -- require 'kickstart.plugins.debug',
 -- require 'kickstart.plugins.indent_line',
 -- require 'kickstart.plugins.lint',
--- require 'kickstart.plugins.formatting',
+require 'kickstart.plugins.formatting',
 require 'kickstart.plugins.autopairs',
 require 'kickstart.plugins.neo-tree',
 require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
